cc1464b727b500beecd6f51fb9162578b50c0d10
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-05-08 22:10:47 -0600

    Update index.tsx

--- qr/src/pages/index.tsx
+++ qr/src/pages/index.tsx
@@ -1,26 +1,41 @@
 import { useState } from 'react';
 import QRCodeComponent from './api/QR/QR-Generator';
-import QRScanner from './api/QR/QR-Scanner';
+import { QrScanner } from '@yudiel/react-qr-scanner';
 
 export default function Home() {
-  const [qrValue, setQRValue] = useState('');
+  const [qrValue, setQRValue] = useState('HOLA BUENAS');
+  const [scannedValue, setScannedValue] = useState('');
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     setQRValue(event.target.value);
   }
 
+  function handleScanResult(result: string | null) {
+    if (result) {
+      setScannedValue(result);
+    }
+  }
+
   return (
-    <div className="flex justify-center items-center h-full bg-gray-800 pb-10">
-      <div className="w-4/6 flex flex-col justify-center items-center mt-3.5">
+    <div className="flex justify-center items-center h-full bg-gray-800">
+      <div className="w-4/6 flex flex-col justify-center items-center">
         <div className="flex justify-center items-center">
           <QRCodeComponent value={qrValue} />
         </div>
-        <div className="w-full flex justify-center items-center m-5">
-          <input type="text" value={qrValue} onChange={handleInputChange} placeholder='Digite el texto a generar' className="w-full bg-green-500 text-black placeholder-black py-2 px-4 rounded-md" />
-        </div>
         <div className="w-full flex justify-center items-center">
-          <QRScanner />
+          <input type="text" value={qrValue} onChange={handleInputChange} className="w-full bg-gray-700 text-white py-2 px-4 rounded-md placeholder-gray-400" placeholder="Introduce tu texto aquí" />
         </div>
+        <div className="w-11/12 flex justify-center items-center">
+          <QrScanner
+            onDecode={handleScanResult}
+            onError={(error) => console.log(error?.message)}
+          />
+        </div>
+        {scannedValue && (
+          <div className="mt-4 bg-green-600">
+            <span className="text-white font-bold">Valor escaneado:</span> {scannedValue}
+          </div>
+        )}
       </div>
     </div>
   );
